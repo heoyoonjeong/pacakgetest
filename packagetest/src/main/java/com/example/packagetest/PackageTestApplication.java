07679882606e46784bfcddb76cb615f674f8e36d
heoyunjeong <prhyj0227@krafton.com>
2022-04-11 16:14:28 +0900

    test

---
+++ packagetest/src/main/java/com/example/packagetest/PackageTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.packagetest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageTestApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
